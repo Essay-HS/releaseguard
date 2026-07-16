af2912013932edf8c7b0b3d6f57379008e201fd7
A C <ac@As-Mac-mini.lan>
2026-07-16 02:08:39 -0500

    Support Twilio trial SMS template

--- scripts/send_twilio_alert.py
+++ scripts/send_twilio_alert.py
@@ -14,10 +14,13 @@ def main():
     parser.add_argument("status", choices=["SUCCESS", "FAILED", "UNSTABLE", "ABORTED"])
     args = parser.parse_args()
     message = create_build_message(os.getenv("BUILD_NUMBER", "unknown"), args.status, os.getenv("PASSED_TESTS", 0), os.getenv("FAILED_TESTS", 0), os.getenv("BUILD_URL", ""))
+    message_to_send = message
+    if os.getenv("TWILIO_TRIAL_MODE", "false").lower() == "true":
+     message_to_send = "sms_internal_alerts"
     if os.getenv("TWILIO_DRY_RUN", "false").lower() == "true":
-        print(message)
+        print(message_to_send)
     else:
-        sent = send_sms(message)
+        sent = send_sms(message_to_send)
         print(f"Sent Twilio message {sent.sid}")
 
 
